4bb6cff4f0ec779fd53349d388aad3d85fa41387
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-17 18:27:41 -0800

    Built Player object with first few properties/functions: name, plays, play(word), totalScore(), hasWon(). Built Jasmine tests for Player class.

--- player.js
+++ player.js
@@ -1,5 +1,37 @@
+var Scrabble = require('./scrabble');
+
+var scrabble = new Scrabble();
+
 var Player = function(name) {
   this.name = name;
+  this.plays = [];
 };
+
+Player.prototype.play = function(word) {
+  // @TODO - restrict non-string or empty string Input
+  this.plays.push(word);
+
+  // using same logic as from Ruby Scrabble for return value: if won, return false. if not won, return word score
+  return ( this.hasWon() === true ) ? false : scrabble.score(word) ;
+};
+
+Player.prototype.totalScore = function() {
+  var score = 0;
+  this.plays.forEach(function(word) {
+    score += scrabble.score(word);
+  });
+  return score;
+};
+
+Player.prototype.hasWon = function() {
+  if (this.totalScore() > 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// highestScoringWord(): Function which returns the highest scoring word the user has played
+// highestWordScore(): Function which returns the highestScoringWord score
 
 module.exports = Player;
--- player.spec.js
+++ player.spec.js
@@ -4,4 +4,83 @@ describe('Player', function() {
   it('is working', function() {
     expect(true).toBe(true);
   });
+
+  it('should be able to instantiate a new Player and return name', function() {
+    var player = new Player("Annalise");
+    expect(player.name).toEqual("Annalise");
+  });
+
+  it('should be able to instantiate a new Player and return name', function() {
+    var player = new Player("Annalise");
+    expect(player.name).toEqual("Annalise");
+  });
+
+  // // @TODO - don't allow null name
+  xit('should not be able to instantiate a new Player without name', function() {
+    var player = new Player();
+    // throw errro
+  });
+
+  it("should be able to play a new word if Player hasn't already won; word score should be returned", function() {
+    var player = new Player("Annalise");
+    expect(player.plays).toEqual([]);
+    expect(player.play("cat")).toEqual(5);
+    expect(player.plays).toEqual(["cat"]);
+  });
+
+  it('.play should return false if player has already won', function() {
+    var player = new Player("Annalise");
+    player.plays = ["cat", "MUZJIKS", "JX"]; // totalScore() = 100
+    expect(player.hasWon()).toBe(false);
+    expect(player.play("a")).toBe(false);
+    expect(player.hasWon()).toBe(true);
+  });
+
+  it('.totalScore() should return 0 if no words played', function() {
+    var player = new Player("Annalise");
+    expect(player.totalScore()).toEqual(0);
+  });
+
+
+  it('.totalScore() should return correct score if plays > 0', function() {
+    var player = new Player("Annalise");
+    expect(player.totalScore()).toEqual(0);
+    player.play("cat");
+    expect(player.totalScore()).toEqual(5);
+    player.play("MUZJIKS");
+    expect(player.totalScore()).toEqual(84);
+  });
+
+  it('.hasWon() should return false if score <= 100', function() {
+    var player = new Player("Annalise");
+    expect(player.totalScore()).toEqual(0);
+    expect(player.hasWon()).toBe(false);
+    player.play("cat"); // totalScore() = 5
+    expect(player.hasWon()).toBe(false);
+    player.play("MUZJIKS"); // totalScore() = 84
+    expect(player.hasWon()).toBe(false);
+    player.play("JX"); // totalScore() = 100
+    expect(player.totalScore()).toEqual(100);
+    expect(player.hasWon()).toBe(false);
+  });
+
+  it('.hasWon() should return true if score > 100', function() {
+    var player = new Player("Annalise");
+    expect(player.totalScore()).toEqual(0);
+    expect(player.hasWon()).toBe(false);
+    player.plays = ["cat", "MUZJIKS", "QZ"]; // totalScore() = 104
+    expect(player.totalScore()).toEqual(104);
+    expect(player.hasWon()).toBe(true);
+  });
+
+  // it('is working', function() {
+  //   expect(true).toBe(true);
+  // });
+  //
+
+  // it('is working', function() {
+  //   expect(true).toBe(true);
+  // });
+  //
+  //
 });
